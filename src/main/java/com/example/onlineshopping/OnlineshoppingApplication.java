d672e08a7940ad8718019080682fccc227db6989
xuehuan <18791643085@163.com>
2018-11-29 11:13:48 +0800

    create project

---
+++ src/main/java/com/example/onlineshopping/OnlineshoppingApplication.java
@@ -0,0 +1,12 @@
+package com.example.onlineshopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineshoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineshoppingApplication.class, args);
+    }
+}
